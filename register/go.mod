5472c5bf33e6af0f30d3a97e85ee769addfcff26
Sethukumar J <sethukumarj.76@gmail.com>
2022-11-13 21:38:47 +0530

    modandsum

---
+++ register/go.mod
@@ -0,0 +1,8 @@
+module reg
+
+go 1.19
+
+require (
+	github.com/go-sql-driver/mysql v1.6.0 // indirect
+	golang.org/x/crypto v0.2.0 // indirect
+)
--- register/main.go
+++ register/main.go
@@ -1,31 +1,127 @@
 package main
 
 import (
-
 	"database/sql"
 	"fmt"
 	"html/template"
 	"net/http"
 	"unicode"
 
-	"golang.org/x/crypto/bcypt"
-	_ "github.com/go-sql-driven/mysql"
-
-)
-
-
+	
+	"golang.org/x/crypto/bcrypt"
+	"github.com/go-sql-driver/mysql"
+	
+	) 
+// "github.com/go-sql-driver/mysql v1.6.0" is not a valid import path
 var tpl *template.Template
 var db *sql.DB
 
-func main (){
+func main() {
 
-	tpl= template.Must(template.ParseGlob("templates/*.html"))
+	tpl = template.Must(template.ParseGlob("templates/*.html"))
 
 	var err error
 
-	db, err  = sql.Open("mysql", "root:password@tcp(localhost:3306)/testdb")
+	db, err = sql.Open("mysql", "root:password@tcp(localhost:3306)/testdb")
 	if err != nil {
+
+		panic(err.Error())
+
+	}
+	defer db.Close()
+	http.HandleFunc("/register", registerHandler)
+	http.HandleFunc("/registerauth", registerAuthHandler)
+	http.ListenAndServe("localhost:8080", nil)
+
+}
+
+// registerhandler serves form for registering new users
+func registerHandler(w http.ResponseWriter, r *http.Request) {
+
+	fmt.Println("**********RegisterHandler running*********")
+	tpl.ExecuteTemplate(w, "register.html", nil)
+}
+
+func registerAuthHandler(w http.ResponseWriter, r *http.Request) {
+
+	fmt.Println("*************registrationHandler running******")
+	r.ParseForm()
+	username := r.FormValue("username")
+
+	nameAlphaNumeric := true
+
+	for _, char := range username {
+
+		if unicode.IsLetter(char) == false && unicode.IsNumber(char) == false {
+			nameAlphaNumeric = false
+		}
 		
 	}
+	//check username pswdlength
+	var nameLength bool
 
+	if 5 <= len(username) && len(username) <= 50 {
+		nameLength = true
+	}
+
+	password := r.FormValue("password")
+	fmt.Println("password:", password, "\npswdLength:", len(password))
+	//variables that mush pass for password creation criteria
+	var pswdLowercase, pswdUppercase, pswdNumber,pswdSpecial,pswdLength,paswdNoSpaces bool
+	pswdNoSpaces = true
+
+	for _, char := range password {
+
+		switch {
+		case unicode.IsLower(char):
+			pswdLowercase = true
+			
+		case unicode.IsUpper(char):
+			pswdUppercase = true
+
+		case unicode.IsNumber(char):
+			pswdNumber = true
+
+		case unicode.IsPunct(char) || unicode.IsSymbol(char):
+			pswdSpecial = true
+
+		case unicode.IsSpace(int32(char)):
+			paswdNoSpaces = false
+		}
+	}
+
+
+	if 11 < len(password) && len(password) < 60 {
+		pswdLength = true
+	}
+
+	fmt.Println("pswdLowercase : ",pswdLowercase, "\npswdUppercase :", pswdUppercase, "\npswdNumber :", pswdNumber,
+"\npswdSpecial :", pswdSpecial, "\npasswordNoSpaces :",paswdNoSpaces)
+
+if !pswdLowercase || !pswdUppercase || !pswdNumber || !pswdLength || !pswdLength || !paswdNoSpaces {
+	tpl.ExecuteTemplate(w , "register.html","please check username and password criteria")
+	return
+}
+
+//check if username already exists for availability
+
+stmt := "SELECT UserId From bcrypt Where username = ?"
+
+row := db.QueryRow(stmt, username)
+
+var uID string
+err := row.Scan(&uID)
+if err != sql.ErrNoRows {
+	fmt.Println("username already exists, err: ", err)
+	tpl.ExecuteTemplate(w, "register.html","username already taken")
+	return
+}
+
+
+var hash []byte
+
+hash, err = bcrypt.GenerateFromPassWord([]byte(password),bcrypt.DefaultCost)
+if err != nil {
+	fmt.Println("bcrypt err: ",err)
+}
 }
