11a0163b8bb8d765d690dcc7629e0c5924047027
SethukumarJ <sethukumarj.76@gmail.com>
2022-11-21 16:43:44 +0530

    Tried connecting postgres with code and getting an error like this- is not officially supported, running under compatibility mode. 2022/11/21 16:39:33 sql: unknown driver  (forgotten import?)

--- book_keeper/main.go
+++ book_keeper/main.go
@@ -1,8 +1,57 @@
 package main
 
-import "github.com/jinzhu/gorm"
+import (
+	"fmt"
+	"log"
+	"os"
 
-type Person struct { 'Person' is unused (deadcode)
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/postgres"
+)
+
+type Person struct {
 	gorm.Model
-	
+
+	Name  string
+	Email string
+	Books []Book
+}
+
+type Book struct {
+	gorm.Model
+
+	Title      string
+	Auther     string
+	CallNumber int
+	PersonID   int
+}
+
+var db *gorm.DB
+var err error
+
+func main() {
+
+	// Loading environmant variables
+
+	dialect := os.Getenv("DIALECT")
+	host := os.Getenv("HOST")
+	dbPort := os.Getenv("DBPORT")
+	user := os.Getenv("USER")
+	dbName := os.Getenv("NAME")
+	password := os.Getenv("PASSWORD")
+
+	// Database connection string
+	dbURI := fmt.Sprintf("host=%s user=%s dbname=%s sslmode=disable password=%s dbPort=%s", host, user, dbName, password, dbPort)
+
+	//openning connection to database
+
+	db, err = gorm.Open(dialect, dbURI)
+	if err != nil {
+		log.Fatal(err)
+	} else {
+
+		fmt.Println("successfully connected!")
+
+	}
+
 }
