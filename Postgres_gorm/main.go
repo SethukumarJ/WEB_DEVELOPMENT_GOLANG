b0da2998033d1de7850d450761fc51387bd67918
SethukumarJ <sethukumarj.76@gmail.com>
2022-11-26 14:58:09 +0530

    filled storage directory

--- Postgres_gorm/main.go
+++ Postgres_gorm/main.go
@@ -73,6 +73,14 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
+	config := storage.Config{
+		Host: os.Getenv("DB_HOST"),
+		Port: os.Getenv("DB_PORT"),
+		User: os.Getenv("DB_USER"),
+		Password: os.Getenv("DB_PASSWORD"),
+		DBName: os.Getenv("DB_NAME"),
+		SSLMode: os.Getenv("DB_SSLMODE"),
+	}
 
 	db, err := storage.NewConnection(config)
 
