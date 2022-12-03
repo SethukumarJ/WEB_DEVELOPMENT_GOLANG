5928ae63e2b684b3a12f95da7c3fda5958d59478
SethukumarJ <sethukumarj.76@gmail.com>
2022-12-03 22:28:07 +0530

    password verification done.

--- Postgres_gorm/controllers/controllers.go
+++ Postgres_gorm/controllers/controllers.go
@@ -28,3 +28,14 @@ func HashPassword(password string) string {
 	}
 	return string(bytes)
 }
+
+
+func VerifyPassword(userPassword string, providedPassword string) bool {
+	err := bcrypt.CompareHashAndPassword([]byte(providedPassword), []byte(userPassword))
+	check := true
+	if err != nil {
+		check = false
+	}
+	return check
+
+}
